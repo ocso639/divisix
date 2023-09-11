e4ba5bc97b12ebf554016ef623bde75c0e40191b
proto <47378885+ocso639@users.noreply.github.com>
2023-09-11 13:42:23 +0000

    Added Kernel Basics

---
+++ types.h
@@ -0,0 +1,16 @@
+#pragma once
+#define NULL    ((void*)0)
+#define TRUE    1
+#define FALSE   0
+
+typedef char                int8_t;
+typedef unsigned char       uint8_t;
+typedef short               int16_t;
+typedef unsigned short      uint16_t;
+typedef int                 int32_t;
+typedef unsigned int        uint32_t;
+typedef unsigned long int   uint64_t;
+typedef unsigned long long  uintptr_t;
+typedef uint64_t            size_t;
+typedef uint32_t            boolean_t;
+typedef const char*         string_t;
